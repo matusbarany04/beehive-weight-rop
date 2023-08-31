9929d28f739c8c03f03268aded007ab75c321bcd
Martin Belej <mbelej100@gmail.com>
2023-08-31 16:24:02 +0200

    svelte-kit-spring

--- src/main/java/com/buzzybees/master/controllers/LoginController.java
+++ src/main/java/com/buzzybees/master/controllers/LoginController.java
@@ -1,14 +1,32 @@
 package com.buzzybees.master.controllers;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@Controller
 public class LoginController {
 
     @GetMapping("/")
-    public String home(HttpServletRequest request) {
-        request.
+    public String home(@CookieValue(value = "sessionid", defaultValue = "") String ssid) {
+        if(ssid.length() == 0) return "redirect:/login";
+        else return "redirect:/dashboard";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+
+        return "";
     }
 }
