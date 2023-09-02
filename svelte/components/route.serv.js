ed8025bf9084b28fe57d6be6871ef4a279933274
Matúš Bárány <58101335+matusbarany04@users.noreply.github.com>
2023-09-02 14:19:40 +0200

    merge

--- svelte/components/route.serv.js
+++ svelte/components/route.serv.js
@@ -1,14 +1,47 @@
+/**
+ * This module provides utilities for routing and navigation within a Svelte application.
+ * It exports a writable Svelte store (`route`) to keep track of the current route,
+ * as well as helper functions to navigate programmatically.
+ */
 import { writable } from 'svelte/store';
+import { prefix } from './prefix.js';
 
+/**
+ * A writable Svelte store that holds the current application's route (path).
+ * Initialized with the current window's location pathname.
+ */
 export const route = writable(window.location.pathname);
 
-// Listen to popstate events, which are fired when navigating with the browser's back/forward buttons
+// Subscribe to prefix changes and update the `currentPrefix` accordingly.
+let currentPrefix = '';
+prefix.subscribe(value => {
+  console.log(value);
+  currentPrefix = value || '';
+});
+
+/**
+ * An event listener that updates the `route` store when the browser's back/forward buttons are used.
+ */
 window.addEventListener('popstate', () => {
   route.set(window.location.pathname);
 });
 
-// Function to navigate programmatically
+/**
+ * Navigates to a given path programmatically.
+ *
+ * @param {string} path - The path to navigate to.
+ */
 export function navigate(path) {
   window.history.pushState({}, '', path);
   route.set(path);
+}
+
+/**
+ * Navigates to a given path programmatically, but with the added `currentPrefix`.
+ * Useful for apps with base path or dynamic segment in the route.
+ *
+ * @param {string} path - The path to navigate to after the prefix.
+ */
+export function navigateWithPrefix(path) {
+  navigate(`${currentPrefix}${path}`)
 }
