dda49ab9bc6f30a7cca1534f00de1325c1eb34fd
Matúš Bárány <58101335+matusbarany04@users.noreply.github.com>
2023-09-14 22:37:51 +0200

    feat: homepage

--- svelte/general/app/components/NavigationBar.svelte
+++ svelte/general/app/components/NavigationBar.svelte
@@ -11,82 +11,98 @@
   };
 
   let pages = [
-    {name: "Domov", link: "/", selected: true},
-    {name: "Panel", link: "/panel", selected: false},
-    {name: "Kontakt", link: "/kontakt", selected: false},
-    {name: "Doprava", link: "/doprava", selected: false},
-    {name: "O nás", link: "/o-nas", selected: false},
-    {name: "Obchod", link: "/obchod", selected: false},
-    {name: "Dokumentácia", link: "/dokumentacia", selected: false}
+    { name: "Domov", link: "/", selected: true },
+    { name: "Panel", link: "/panel", selected: false },
+    { name: "Kontakt", link: "/kontakt", selected: false },
+    { name: "Doprava", link: "/doprava", selected: false },
+    { name: "O nás", link: "/o-nas", selected: false },
+    { name: "Obchod", link: "/obchod", selected: false },
+    { name: "Dokumentácia", link: "/dokumentacia", selected: false },
   ];
-
-
 </script>
 
-<svelte:window on:resize={resize}/>
+<svelte:window on:resize={resize} />
 
 <nav class="bg-gray-800">
   <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
     <div class="relative flex h-16 items-center justify-between">
       <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
         <!-- Mobile menu button-->
-        <button type="button"
-                class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                aria-controls="mobile-menu" aria-expanded="false" on:click={()=>{toggle = !toggle}}>
+        <button
+          type="button"
+          class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+          aria-controls="mobile-menu"
+          aria-expanded="false"
+          on:click={() => {
+            toggle = !toggle;
+          }}
+        >
           <span class="absolute -inset-0.5"></span>
           <span class="sr-only">Open main menu</span>
 
-          <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-               aria-hidden="{toggle}">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
+          <svg
+            class="block h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            aria-hidden={toggle}
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
           </svg>
-
         </button>
       </div>
-      <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+      <div
+        class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start"
+      >
         <div class="flex flex-shrink-0 items-center">
-          <img class="h-8 w-auto" src="/img/beeman.png"
-               alt="BuzzyBees">
+          <img class="h-8 w-auto" src="/img/beeman.png" alt="BuzzyBees" />
         </div>
         <div class="hidden sm:ml-6 sm:block">
-          <div class="flex space-x-4">
+          <div class="flex flex-wrap space-x-4">
             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
 
-
             {#each pages as page}
-              <a href="{page.link}" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
-                >{page.name}</a>
+              <a
+                href={page.link}
+                class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+                >{page.name}</a
+              >
             {/each}
           </div>
         </div>
       </div>
-      <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-        <button type="button"
-                class="relative rounded-full bg-gray-800 p-1  ">
-
-
-          <RouterLink url="/login"><span
-            class="text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">Prihlásiť sa</span>
+      <div
+        class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
+      >
+        <button type="button" class="relative rounded-full bg-gray-800 p-1">
+          <RouterLink url="/login"
+            ><span
+              class="text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+              >Prihlásiť sa</span
+            >
           </RouterLink>
         </button>
-
-
       </div>
     </div>
   </div>
 
   <!-- Mobile menu, show/hide based on menu state. -->
   {#if toggle}
-
     <div class="absolute sm:hidden bg-primary-100 w-full" id="mobile-menu">
       <div class="space-y-1 px-2 pb-3 pt-2">
-  
         {#each pages as page}
-          <a href="{page.link}" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-          >{page.name}</a>
+          <a
+            href={page.link}
+            class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+            >{page.name}</a
+          >
         {/each}
-        
-     </div>
+      </div>
     </div>
   {/if}
 </nav>
--- svelte/general/pages/Home.svelte
+++ svelte/general/pages/Home.svelte
@@ -4,7 +4,7 @@
   import Button from "../../components/Buttons/Button.svelte";
 </script>
 
-<main class="w-screen min-h-screen bg-primary-100">
+<main class="w-screen min-h-screen bg-primary-100 overflow-y-scroll">
   <section id="home" class="h-screen flex flex-col">
     <NavigationBar></NavigationBar>
     <div class="w-full flex-1 flex flex-row">
@@ -13,7 +13,7 @@
           Moderné úľové váhy pre monitorovanie
         </h1>
         <h5 class="text-slate-100 text-xl">
-          BeeVerse váhový systém včelieho úľa je monitorovací systém, ktorý
+          BuzzyBees váhový systém včelieho úľa je monitorovací systém, ktorý
           okamžite informuje včelára o akýchkoľvek väčších zmenách v úli - váhe,
           teplote, vlhkosti a zvuku včiel.
         </h5>
@@ -34,4 +34,54 @@
       <div class="hidden md:block right flex-1"></div>
     </div>
   </section>
+  <section
+    id="statistics"
+    class="w-full min-h-screen flex flex-row items-center justify-center"
+  >
+    <div
+      class="w-full grid grid-cols-1 sm:grid-cols-3 px-10 gap-1 sm:gap-2 md:gap-16"
+    >
+      <div
+        class="m-4 aspect-square rounded-tl-2xl rounded-tr-[50%] rounded-br-[50%] rounded-bl-[50%] bg-confirm-800 flex flex-col justify-center items-center"
+      >
+        <h1 class="text-secondary-500 text-7xl font-bold text-center">100%</h1>
+        <p class="text-4xl text-slate-100 text-center sm:text-2xl">
+          Spokojných včelárov
+        </p>
+      </div>
+      <div
+        class="aspect-square rounded-2xl bg-confirm-800 flex flex-col justify-center items-center m-4"
+      >
+        <h1 class="text-secondary-500 text-7xl font-bold text-center">25%</h1>
+        <p class="text-4xl text-slate-100 text-center sm:text-2xl">
+          Zvýšená produktivita
+        </p>
+      </div>
+      <div
+        class="m-4 aspect-square rounded-tl-[50%] rounded-2xl bg-confirm-800 flex flex-col justify-center items-center"
+      >
+        <h1 class="text-secondary-500 text-7xl font-bold text-center">99%</h1>
+        <p class="text-4xl text-slate-100 text-center sm:text-2xl">
+          Garantovaná Spoľhalivosť
+        </p>
+      </div>
+    </div>
+  </section>
+
+  <section
+    id="join"
+    class="w-full min-h-min flex flex-col items-center justify-center px-8"
+  >
+    <h1 class="text-7xl font-bold text-slate-100 text-center">
+      Pridajte sa k úľu!
+    </h1>
+    <p class="mb-4 max-w-3xl text-slate-100 text-2xl text-center">
+      Posuň svoje včelárstvo na vyššiu úroveň s najnovšími technológiami na
+      riadenie úľov. Nepremeškaj príležitosť na sladšie úrody. Zaži ten
+      vzrušujúci bzukot!
+    </p>
+
+    <Button type="primary" text="Kúpiť váhu"></Button>
+  </section>
+  <section id="spacer" class="h-64"></section>
 </main>
