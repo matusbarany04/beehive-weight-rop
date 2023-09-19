ebc80e03ab7ffa3725d6fbb84af47b2a665b3f5f
Martin Belej <mbelej100@gmail.com>
2023-09-19 23:09:40 +0200

    nastavenia ula

---
+++ svelte/dashboard/app/component/beehives/Sensor.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let name;
+  export let img;
+</script>
+
+<div class="rounded bg-slate-100 text-center w-min p-2 cursor-pointer hover:scale-105">
+  <img class="m-auto" src={img} alt={name} />
+  <div>{name}</div>
+  
+</div>
