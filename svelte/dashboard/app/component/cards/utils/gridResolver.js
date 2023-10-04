055217392c514f4110eb975b606bc275d5599321
Matúš Bárány <58101335+matusbarany04@users.noreply.github.com>
2023-10-04 21:57:17 +0200

    prettier

--- svelte/dashboard/app/component/cards/utils/gridResolver.js
+++ svelte/dashboard/app/component/cards/utils/gridResolver.js
@@ -1,30 +1,23 @@
 /**
- * GridResolver resolves xy grid - 
- * resolver checks if items are not overflowing the grid 
+ * GridResolver resolves xy grid -
+ * resolver checks if items are not overflowing the grid
  * or they are not overlapping each other items have x,y coordinate and w,h as their span
  */
-export class GridResolver{
+export class GridResolver {
+  /**
+   *
+   * @param x {number}
+   * @param y {number}
+   * @param gridItems {array<Item>}
+   * @param item {Item}
+   */
+  static resolve(x, y, gridItems, item) {}
 
-    /**
-     * 
-     * @param x {number}
-     * @param y {number}
-     * @param gridItems {array<Item>}
-     * @param item {Item}
-     */
-    static resolve(x,y, gridItems, item) {
-        
-    }
-
-    /**
-     * 
-     * @param x {number}
-     * @param y {number}
-     * @param gridItems {array<Item>}
-     */
-    static isPossible(x,y, gridItems){
-        
-        
-    }
-
+  /**
+   *
+   * @param x {number}
+   * @param y {number}
+   * @param gridItems {array<Item>}
+   */
+  static isPossible(x, y, gridItems) {}
 }
