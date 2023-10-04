b13e8d23e16199931473d7f7a6d6311c6a9c9920
Matúš Bárány <58101335+matusbarany04@users.noreply.github.com>
2023-10-04 21:56:40 +0200

    feat: animations

---
+++ svelte/dashboard/app/component/cards/utils/gridResolver.js
@@ -0,0 +1,30 @@
+/**
+ * GridResolver resolves xy grid - 
+ * resolver checks if items are not overflowing the grid 
+ * or they are not overlapping each other items have x,y coordinate and w,h as their span
+ */
+export class GridResolver{
+
+    /**
+     * 
+     * @param x {number}
+     * @param y {number}
+     * @param gridItems {array<Item>}
+     * @param item {Item}
+     */
+    static resolve(x,y, gridItems, item) {
+        
+    }
+
+    /**
+     * 
+     * @param x {number}
+     * @param y {number}
+     * @param gridItems {array<Item>}
+     */
+    static isPossible(x,y, gridItems){
+        
+        
+    }
+
+}
--- svelte/dashboard/app/component/cards/utils/item.js
+++ svelte/dashboard/app/component/cards/utils/item.js
@@ -1,192 +1,195 @@
-import { generateUUID } from "../../../../../components/lib/utils/staticFuncs";
+import {generateUUID} from "../../../../../components/lib/utils/staticFuncs";
 
 export class Item {
-  id;
-  _x; //in relative pos
-  _y;
-  _w;
-  _h;
-  _xCoordinate; // inPixelValues
-  _yCoordinate;
-  _pixelWidth;
-  _pixelHeight;
-  _unitSize = 1;
-  _mounted = false;
-  _draggable = false;
-  _padding = 0;
+    id;
+    _x; //in relative pos
+    _y;
+    _w;
+    _h;
+    _xCoordinate = 0; // inPixelValues
+    _yCoordinate = 0;
+    _pixelWidth = 0;
+    _pixelHeight = 0;
+    _unitSize = 1;
+    _mounted = false;
+    _draggable = false;
+    _padding = 0;
 
-  constructor(x, y, w, h) {
-    this.id = generateUUID();
-    this._x = x;
-    this._y = y;
-    this._w = w;
-    this._h = h;
-    this.calculateSize();
-  }
+    constructor(x, y, w, h) {
+        this.id = generateUUID();
+        this._x = x;
+        this._y = y;
+        this._w = w;
+        this._h = h;
+        this.calculateSize();
+    }
 
-  set unitSize(value) {
-    this._unitSize = value;
-    this.calculateSize();
-    this.callback();
-  }
+    set unitSize(value) {
+        this._unitSize = value;
+        this.calculateSize();
+        this.callback();
+    }
 
-  get unitSize() {
-    return this._unitSize;
-  }
+    get unitSize() {
+        return this._unitSize;
+    }
 
-  set padding(value) {
-    this._padding = value;
-    this.calculateSize();
-    this.callback();
-  }
+    set padding(value) {
+        this._padding = value;
+        this.calculateSize();
+        this.callback();
+    }
 
-  //BUG padding missing
-  calculateSize() {
-    this._pixelHeight =
-      this._padding * (this._h - 1) + this._unitSize * this._h;
-    this._pixelWidth = this._padding * (this._w - 1) + this._unitSize * this._w;
-  }
+    //BUG padding missing
+    calculateSize() {
+        this._pixelHeight =
+            this._padding * (this._h - 1) + this._unitSize * this._h;
+        this._pixelWidth = this._padding * (this._w - 1) + this._unitSize * this._w;
+    }
 
-  calculatePos(padding) {
-    this._xCoordinate = this._unitSize * this._x;
-    this._yCoordinate = this._unitSize * this._y;
-  }
+    calculatePos(padding) {
+        this._xCoordinate = this._unitSize * this._x;
+        this._yCoordinate = this._unitSize * this._y;
+    }
 
-  get x() {
-    return this._x;
-  }
+    get x() {
+        return this._x;
+    }
 
-  get pixelHeight() {
-    return this._pixelHeight;
-  }
+    get pixelHeight() {
+        return this._pixelHeight;
+    }
 
-  /**
-   *
-   * @param value {boolean}
-   */
-  set mounted(value) {
-    this._mounted = value;
-    this.mountedChangedEvent();
-  }
+    /**
+     *
+     * @param value {boolean}
+     */
+    set mounted(value) {
+        this._mounted = value;
+        this.mountedChangedEvent();
+    }
 
-  /**
-   *
-   * @return {boolean}
-   */
-  get mounted() {
-    return this._mounted;
-  }
+    /**
+     *
+     * @return {boolean}
+     */
+    get mounted() {
+        return this._mounted;
+    }
 
-  mountedChangedEvent = () => {};
+    mountedChangedEvent = () => {
+    };
 
-  subscribeMounted(event) {
-    this.mountedChangedEvent = event;
-  }
+    subscribeMounted(event) {
+        this.mountedChangedEvent = event;
+    }
 
-  set pixelHeight(value) {
-    this._pixelHeight = value;
-    this.callback();
-  }
+    set pixelHeight(value) {
+        this._pixelHeight = value;
+        this.callback();
+    }
 
-  get pixelWidth() {
-    return this._pixelWidth;
-  }
+    get pixelWidth() {
+        return this._pixelWidth;
+    }
 
-  set pixelWidth(value) {
-    this._pixelWidth = value;
-    this.callback();
-  }
+    set pixelWidth(value) {
+        this._pixelWidth = value;
+        this.callback();
+    }
 
-  set x(value) {
-    this._x = value;
-    this.callback();
-    this.calculateSize();
-  }
+    set x(value) {
+        this._x = value;
+        this.calculateSize();
+        this.callback();
+    }
 
-  get y() {
-    return this._y;
-  }
+    get y() {
+        return this._y;
+    }
 
-  set y(value) {
-    this._y = value;
-    this.callback();
-    this.calculateSize();
-  }
+    set y(value) {
+        this._y = value;
+        this.calculateSize();
+        this.callback();
+    }
 
-  get w() {
-    return this._w;
-  }
+    get w() {
+        return this._w;
+    }
 
-  set w(value) {
-    this._w = value;
-    this.callback();
-    this.calculateSize();
-  }
+    set w(value) {
+        this._w = value;
+        this.calculateSize();
+        this.callback();
+    }
 
-  get h() {
-    return this._h;
-  }
+    get h() {
+        return this._h;
+    }
 
-  set h(value) {
-    this._h = value;
-    this.callback();
-    this.calculateSize();
-  }
+    set h(value) {
+        this._h = value;
+        this.calculateSize();
+        this.callback();
+    }
 
-  /**
-   * Special setter when in need to set both at the same time,
-   * It can happen that second one won't be reflected when calling separately
-   * @param value {Object}
-   * @param value.w {number}
-   * @param value.h {number}
-   * */
-  set wh(value) {
-    this._w = value.w;
-    this._h = value.h;
-    this.callback();
-    this.calculateSize();
-  }
+    /**
+     * Special setter when in need to set both at the same time,
+     * It can happen that second one won't be reflected when calling separately
+     * @param value {Object}
+     * @param value.w {number}
+     * @param value.h {number}
+     * */
+    set wh(value) {
+        this._w = value.w;
+        this._h = value.h;
+        this.calculateSize();
+        this.callback();
+    }
 
-  get xCoordinate() {
-    return this._xCoordinate;
-  }
+    get xCoordinate() {
+        return this._xCoordinate;
+    }
 
-  set xCoordinate(value) {
-    this._xCoordinate = value;
-    this.callback();
-  }
+    set xCoordinate(value) {
+        this._xCoordinate = value;
+        this.callback();
+    }
 
-  get yCoordinate() {
-    return this._yCoordinate;
-  }
+    get yCoordinate() {
+        return this._yCoordinate;
+    }
 
-  set yCoordinate(value) {
-    this._yCoordinate = value;
-    this.callback();
-  }
+    set yCoordinate(value) {
+        this._yCoordinate = value;
+        this.callback();
+    }
 
-  callback = () => {};
+    callback = () => {
+    };
 
-  setValueChangedCallback(callback) {
-    this.callback = callback;
-  }
+    setValueChangedCallback(callback) {
+        this.callback = callback;
+    }
 
-  /**
-   *
-   * @param value {boolean}
-   */
-  set draggable(value) {
-    this._draggable = value;
-    this.draggableChangedEvent(this._draggable);
-  }
+    /**
+     *
+     * @param value {boolean}
+     */
+    set draggable(value) {
+        this._draggable = value;
+        this.draggableChangedEvent(this._draggable);
+    }
 
-  get draggable() {
-    return this._draggable;
-  }
+    get draggable() {
+        return this._draggable;
+    }
 
-  draggableChangedEvent = () => {};
+    draggableChangedEvent = () => {
+    };
 
-  setDraggableChangedEvent(event) {
-    this.draggableChangedEvent = event;
-  }
+    setDraggableChangedEvent(event) {
+        this.draggableChangedEvent = event;
+    }
 }
