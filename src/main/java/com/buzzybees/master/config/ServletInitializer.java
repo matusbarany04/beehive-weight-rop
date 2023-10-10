bb3e1c7613d90d082402b032a64270b898fe15f1
MARTINB005 <mbelej100@gmail.com>
2023-10-10 12:11:18 +0200

    jsessionid

--- src/main/java/com/buzzybees/master/config/ServletInitializer.java
+++ src/main/java/com/buzzybees/master/config/ServletInitializer.java
@@ -1,13 +1,27 @@
 package com.buzzybees.master.config;
 
 import com.buzzybees.master.MasterApplication;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.SessionCookieConfig;
+import jakarta.servlet.SessionTrackingMode;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Collections;
+
 @Configuration
 public class ServletInitializer extends SpringBootServletInitializer {
 
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+
+        servletContext.setSessionTrackingModes(Collections.singleton(SessionTrackingMode.COOKIE));
+        SessionCookieConfig sessionCookieConfig = servletContext.getSessionCookieConfig();
+        sessionCookieConfig.setHttpOnly(true);
+    }
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(MasterApplication.class);
