80ba20292c08ce559c6a2d655f09f81fc3220c3a
MartinB005 <79859750+MartinB005@users.noreply.github.com>
2023-11-11 19:28:22 +0100

    Update constants.h

--- hardware/src/constants.h
+++ hardware/src/constants.h
@@ -2,8 +2,8 @@
 #define BEEHIVE_ID "NY17IS0J9RKMRFP3"
 #define MODEL_NAME "BBIMZ-A"
 
-#define WIFI_SSID "SNPD"
-#define WIFI_PASSWORD "ke257-NT_61_ab"
+#define WIFI_SSID ""
+#define WIFI_PASSWORD "☺"
 
 #define SDA_PIN 22
 #define SCL_PIN 23
