38b242d0df8aed1d39646c94ca4ec739f6b2e469
Matúš Bárány <58101335+matusbarany04@users.noreply.github.com>
2023-11-24 20:17:02 +0100

    quick fix

--- svelte/general/pages/About.svelte
+++ svelte/general/pages/About.svelte
@@ -4,17 +4,17 @@
 </script>
 
 <Root>
-  
-  <AboutCard 
-    name="Martin" 
+  <AboutCard
+    name="Martin"
     description="Lorem ipsum dolor sit amet. Et vitae autem qui distinctio excepturi ut ratione impedit in sunt facilis aut eligendi consectetur. In architecto itaque et delectus sequi ut quia voluptate aut nobis iure."
-    direction="left" 
-    image="/img/authors/martin.jpg"/>
+    direction="left"
+    image="/img/authors/martin.jpg"
+  />
 
-  <AboutCard 
-    name="Matúš" 
+  <AboutCard
+    name="Matúš"
     description="Quo quod dolorum ea eaque veritatis aut fugit beatae eos nobis enim aut consequatur voluptatem rem blanditiis repellat ut necessitatibus facere. Hic totam libero quo dolor distinctio qui iure omnis."
-    direction="right" 
-    image="/img/authors/matus.jpg"/>
-
+    direction="right"
+    image="/img/authors/matus.jpg"
+  />
 </Root>
