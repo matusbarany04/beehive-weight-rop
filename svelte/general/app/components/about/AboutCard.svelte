4fd591a7d8979d03589e7c9ea1befb2506e550ec
Matúš Bárány <58101335+matusbarany04@users.noreply.github.com>
2023-11-25 18:05:38 +0100

    feat: homepage titles

--- svelte/general/app/components/about/AboutCard.svelte
+++ svelte/general/app/components/about/AboutCard.svelte
@@ -18,7 +18,7 @@
     : 'md:flex-row-reverse flex-col'}  mx-auto my-4 min-h-min md:h-64 max-w-4xl rounded-md"
 >
   <div
-    class="mx-5 aspect-square mx-auto  w-5/6 md:w-min md:h-full rounded-full border border-tertiary-100 bg-contain bg-center bg-no-repeat"
+    class="md:mx-5 aspect-square mx-auto  w-5/6 md:w-min md:h-full rounded-full border border-tertiary-100 bg-contain bg-center bg-no-repeat"
     style="background-image: url({image})"
   ></div>
 
--- svelte/general/pages/TestGeneralSite.svelte
+++ svelte/general/pages/TestGeneralSite.svelte
@@ -2,9 +2,17 @@
   import RouterLink from "../../components/RouterLink.svelte";
   import toast from "../../components/Toast/toast";
   import Button from "../../components/Buttons/Button.svelte";
+  import ButtonSmall from "../../components/Buttons/ButtonSmall.svelte";
+  import CircleButton from "../../components/Buttons/CircleButton.svelte";
 </script>
 
+
 <main class="min-h-screen w-full flex-1 bg-primary-200 pt-10">
+  <div class="absolute left-0 top-0 p-4">
+    <CircleButton type="primary" link="/" image="icons/arrow-left.svg"
+    ></CircleButton>
+  </div>
+  <h1 class="text-5xl font-bold text-center text-tertiary-100">Router Link Docs</h1>
   <section class="mx-auto my-4 w-11/12 rounded-lg bg-tertiary-100 p-4">
     <h1 class="my-4 text-4xl font-bold">Router Link</h1>
     <p>
@@ -13,12 +21,12 @@
     </p>
 
     <RouterLink url="/dashboard" baseRoute="true"
-      >Dashboard Home trough RouterLink
+    >Dashboard Home trough RouterLink
     </RouterLink>
 
     <p>That's why we need to use a tag</p>
     <a class="text-error-600 underline" href="/dashboard"
-      >Dashboard Home trough anchor tag</a
+    >Dashboard Home trough anchor tag</a
     >
 
     <pre
