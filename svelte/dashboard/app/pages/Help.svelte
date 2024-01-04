77f8dbba6872b35e429fb01a6cd36dd66963481d
Matúš Bárány <58101335+matusbarany04@users.noreply.github.com>
2024-01-04 10:58:21 +0100

    clean

--- svelte/dashboard/app/pages/Help.svelte
+++ svelte/dashboard/app/pages/Help.svelte
@@ -19,32 +19,8 @@
   />
 </svelte:head>
 
-<SettingsHeader
-  title="Často kladené otázky"
-  description="Nemôžete nájsť odpoveď, ktorú hľadáte? Obráťte sa na náš zákaznícky servis."
-/>
 
-<div class="mx-auto mt-8 w-full divide-neutral-200">
-  <HelpDropdown
-    question="Čo je BuzzyBees?"
-    answer="BuzzyBees je inovatívne riešenie na monitorovanie úľov navrhnuté tak, aby poskytovalo včelárom dáta v reálnom čase o zdraví úľa, produkcii medu, raste plodu a ďalších."
-  />
-  <HelpDropdown
-    question="Ako funguje BuzzyBees?"
-    answer="Využívaním pokročilých senzorov a vlastných algoritmov BuzzyBees meria hmotnosť, teplotu a ďalšie kľúčové parametre vašich úľov a potom tieto informácie prenáša do prístupnej aplikácie alebo panela."
-  />
-  <HelpDropdown
-    question="Prečo je monitorovanie úľa dôležité?"
-    answer="Monitorovanie hmotnosti a podmienok úľa môže pomôcť pri určovaní úrovní produkcie medu, zdravia včiel, rastu plodu, potravinových rezerv a možných včelích rojov. BuzzyBees pomáha zabezpečiť prosperitu vašich včelích kolónií."
-  />
-
-  <HelpDropdown
-    question="Čím sa BuzzyBees líši od iných monitorovacích nástrojov?"
-    answer="BuzzyBees kombinuje starodávne včelárske múdrosti s najmodernejšou technológiou. Naša ľahko použiteľná aplikácia, presné senzory a jedinečné algoritmy nás odlišujú od tradičných monitorovacích riešení."
-  />
-
-  <HelpDropdown
-    question="Sú moje dáta v bezpečí?"
-    answer="Rozhodne. Prioritou je súkromie používateľa a bezpečnosť dát. Všetky prenášané dáta sú šifrované a nikdy nepredávame vaše dáta tretím stranám."
-  />
+<div class="h-full w-full grid place-items-center">
+  
+  <p>Caka sa na spravenie</p>
 </div>
